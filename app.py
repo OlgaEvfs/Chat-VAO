0c15ccf177bdf7a061c140ac4ac8d7adea3692bb
ViktoriaLoiko <loiko.nika@gmail.com>
2025-01-17 12:39:15 +0200

    шапка и футер c bootstrap стилем в base

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, url_for
+'''Импорт фласк. '''
 
 app = Flask(__name__)
 
-
+'''Отслеживание главной страницы'''
 @app.route('/')
 @app.route('/home')
 def index():
@@ -15,6 +16,6 @@ def about():
 
 
 
-
+''' Для запуска приложения. Для отслеживания ошибок добавлен атрибут. False если отключить показ'''
 if __name__ =="__main__":
     app.run(debug=True)
